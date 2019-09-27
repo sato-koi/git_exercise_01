cae5aeafcb9486d61b9fc1039d66ee72c7ebe5b4
sato-koi <sato.koichi.0404@gmail.com>
2019-09-28 00:47:28 +0900

    合計値抽出修正

--- ex01.rb
+++ ex01.rb
@@ -1,4 +1,6 @@
-def plus(sales = { hokkaido: 300, tohoku: 200, kanto: 200, chubu: 300, kinki: 400, chugoku: 180, shikoku: 140, kyushu: 120 })
+sales = { hokkaido: 300, tohoku: 200, kanto: 200, chubu: 300, kinki: 400, chugoku: 180, shikoku: 140, kyushu: 120 }
+
+def plus(sales)
   puts sales.values.inject(:+)
 end
 
