7ce18cae4fcb8962d3cc5dd47ffc38d471753ecc
sato-koi <sato.koichi.0404@gmail.com>
2019-09-27 23:17:06 +0900

    合計値抽出

--- ex01.rb
+++ ex01.rb
@@ -1,7 +1,6 @@
-def choice(sales = { hokkaido: 300, tohoku: 200, kanto: 200, chubu: 300, kinki: 400, chugoku: 180, shikoku: 140, kyushu: 120 })
-  sales.each do |k, v|
-    puts k
-  end
+def plus(sales = { hokkaido: 300, tohoku: 200, kanto: 200, chubu: 300, kinki: 400, chugoku: 180, shikoku: 140, kyushu: 120 })
+  puts sales.values.inject(:+)
 end
 
-choice
+plus
+
