fa2c64bdccdc11785bf1abee9bba258c9df5599c
sato-koi <sato.koichi.0404@gmail.com>
2019-10-01 00:47:04 +0900

    売上300以下地域カウント

--- ex01.rb
+++ ex01.rb
@@ -1,8 +1,9 @@
 sales = { hokkaido: 300, tohoku: 200, kanto: 200, chubu: 300, kinki: 400, chugoku: 180, shikoku: 140, kyushu: 120 }
 
 
-def plus(sales)
-  puts sales.values.inject(:+)
+def less(sales)
+  less_sales = sales.select { |k, v| v <= 300 }
+  puts less_sales.size
 end
 
-plus(sales)
+less(sales)
